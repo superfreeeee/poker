fb4cc128faf3f8c84b3470acab559f42034bd309
superfree <superfreeeee@gmail.com>
2023-05-03 12:37:45 +0800

    revert: clean test code

--- apps/pc/src/App.tsx
+++ apps/pc/src/App.tsx
@@ -1,25 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
 import './others';
 
 const App = () => {
-  const [state] = useState();
-
-  useEffect(() => {
-    console.log('state', state);
-  }, []);
-
-  const name = 'suprefree';
-  let fullName = `__${name}`;
-
-  if (name) {
-    fullName = `${name}__`;
-  }
-
-  const greeting = useCallback(() => {
-    console.log(`fullName = ${fullName}`);
-  }, [fullName]);
-
   return (
     <div className={styles.container}>
       <h1>Poker PC</h1>
