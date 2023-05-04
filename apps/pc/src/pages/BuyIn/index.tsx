b06c74c6c932963d5a461194d61829d5cc45f40e
superfree <superfreeeee@gmail.com>
2023-05-04 21:05:25 +0800

    fix: eslint fix

--- apps/pc/src/pages/BuyIn/index.tsx
+++ apps/pc/src/pages/BuyIn/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 interface IBuyInProps {}
 
-const BuyIn: FC<IBuyInProps> = ({}) => {
+const BuyIn: FC<IBuyInProps> = () => {
   return (
     <div>
       <h1>Buy-in page</h1>
