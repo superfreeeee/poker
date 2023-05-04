c717e8afc87d6c76b55b4bc467837e836f1803b6
superfree <superfreeeee@gmail.com>
2023-05-04 22:28:32 +0800

    test: config jest workers

--- apps/pc/src/utils/type.ts
+++ apps/pc/src/utils/type.ts
@@ -6,7 +6,3 @@
 export const isNumber = (data: unknown): data is number => {
   return typeof data === 'number';
 };
-
-export const isString = (data: unknown): data is string => {
-  return typeof data === 'string';
-};
