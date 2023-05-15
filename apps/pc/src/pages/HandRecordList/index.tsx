eaaed1aef5beea04ea6f064fcfe959a3f2e49b71
superfree <superfreeeee@gmail.com>
2023-05-16 00:01:23 +0800

    feat: add recordList gap

--- apps/pc/src/pages/HandRecordList/index.tsx
+++ apps/pc/src/pages/HandRecordList/index.tsx
@@ -39,7 +39,7 @@ const HandRecordList = () => {
                 className={styles.record}
                 onClick={() => navigate(`/hand/${record.id}`)}
               >
-                <h3 className={styles.title}>Hand Record: {record.id.substring(0, 6)}</h3>
+                <h3 className={styles.title}>Record Id: {record.id.substring(0, 6)}</h3>
                 <div className={styles.time}>{new Date(record.createTime).toLocaleString()}</div>
                 {renderCardText(record.boardCards, styles.cards)}
               </div>
