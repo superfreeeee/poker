1ec90f1c1ccef767871776118433086c80779a91
superfree <superfreeeee@gmail.com>
2023-05-29 13:21:59 +0800

    feat: user profile at Home

--- apps/pc/src/pages/Home/index.tsx
+++ apps/pc/src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Divider } from 'antd';
 import { ArrowRightOutlined } from '@ant-design/icons';
 import { useLoginCheck } from '../../hooks/useLoginCheck';
-import { useCurrentUser } from '../../models/user';
 import GameList from './GameList';
+import UserProfile from './UserProfile';
 import styles from './index.module.scss';
 
 interface IPageLinkProps {
@@ -26,32 +26,22 @@ const PageLink: FC<IPageLinkProps> = ({ title, path }) => {
 const Home = () => {
   useLoginCheck();
 
-  const currentUser = useCurrentUser();
-  const navigate = useNavigate();
-
-  const switchUser = () => {
-    navigate('/login');
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h1>Home</h1>
-        <Button type="primary" danger className={styles.switchUser} onClick={switchUser}>
-          Switch User
-        </Button>
+        <UserProfile />
       </div>
       <div className={styles.content}>
-        <h3>currentUser</h3>
-        <div>id: {currentUser?.id}</div>
-        <div>name: {currentUser?.name}</div>
+        <h3>Game List</h3>
+        <GameList />
+        <Divider />
+        <h3>Dev Navigation</h3>
         <div style={{ marginTop: 12, display: 'flex', alignItems: 'center', gap: 8 }}>
           <h3>Pages:</h3>
-          <PageLink title="HandRecordList" path="/hands" />
           <PageLink title="BuyIn" path="/buyin/create" />
           <PageLink title="RNG" path="/rng" />
         </div>
-        <GameList />
       </div>
     </div>
   );
