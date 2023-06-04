9cdfbd29d255f086670a0901bff5d1290ad5919d
superfree <superfreeeee@gmail.com>
2023-06-04 23:36:05 +0800

    feat: add uuid to headers

--- apps/pc/src/index.tsx
+++ apps/pc/src/index.tsx
@@ -1,25 +1,9 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { nanoid } from 'nanoid';
-import { ELocalStorageKey, getItem, setItem } from './common/localStorage';
+import { initClientId } from './common/env';
 import { logger } from './common/commonLogger';
 import App from './App';
 import './index.module.scss';
-
-/**
- * 初始化设备唯一标识
- * @returns
- */
-function initClientId(): string {
-  let clientId = getItem(ELocalStorageKey.Uuid);
-  if (clientId) {
-    return clientId;
-  }
-
-  clientId = nanoid();
-  setItem(ELocalStorageKey.Uuid, clientId);
-  return clientId;
-}
 
 function createApp() {
   // initial
